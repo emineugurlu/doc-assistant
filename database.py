d62a7a319e524a4f5bc8b4e592e9813fbd89a896
Emine Ugurlu <emineugurlu957@gmail.com>
2026-04-13 16:19:35 +0300

    database.py has been added an sqlite connection adn a document table have been created

--- database.py
+++ database.py
@@ -1,30 +1,38 @@
-from sqlalchemy import create_engine , Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from datetime import datetime
-
 
 VERITABANI_URL = "sqlite:///./doc_assistant.db"
 
 engine = create_engine(
     VERITABANI_URL,
-    connect_args={"chech_same_thread": False} # birden fazla istek gelince çökmemesi için
-
+    connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # veritabanıyla her konuşmada açılan geçici bağlantı 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-Base = declarative_base() #tabloların atası tüm tablolalarımız miras alacak
+class Base(DeclarativeBase):
+    pass
 
-class Dokuman(Base) :
-    class Dokuman(Base) :
-        __tablename__  =   "dokumanlar"
+class Dokuman(Base):
+    __tablename__ = "dokumanlar"
 
-        id = Column(Integer, primary_key=True, index=True)
-        dosya_adi = Column(String(255), nullable=False)
-        dosya_turu =  Column(String(10), nullable=False)
-        icerik = Column(Text, nullable=False)
-        yuklenme_tarihi=Column(DateTime,default=datetime.now)
-    
-    def veritabani_olustur():
-        Base.metadata.create_all(bind=engine) #sql yazmadan tablo oluşturuyor 
+    id = Column(Integer, primary_key=True, index=True)
+    dosya_adi = Column(String(255), nullable=False)
+    dosya_turu = Column(String(10), nullable=False)
+    icerik = Column(Text, nullable=False)
+    yuklenme_tarihi = Column(DateTime, default=datetime.now)
+
+def veritabani_olustur():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI   #fastapi kütüphanesinin içinden fastApı sınıfını al yani pip install ile indirdiğimiz paketi kullanıyoruz.
-
+from database import veritabani_olustur
 app = FastAPI (       #swagger da görünecek
     title="Doc Assistant",
     description="Al destekli doküman analiz aracı",
     version="0.1.0"
 )
 
+veritabani_olustur()
 @app.get("/")   #tarayıcıdan get isteği gelirse ana_sayfa() fonksiyonu çalışır
 def ana_sayfa():
     return{"mesaj" : "Doc Assistant çalışıyor", "durum" : "aktif"} #ve buna geri döndür
